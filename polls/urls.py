5591162ed05f98f91dd9e9e67edb61a19f7d33b2
Sravan Reddy <sravfeyn@gmail.com>
2018-04-23 19:01:30 +0530

    add templates

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import index, sp_index, detail
+from .views import index, detail
 
 urlpatterns = [
 	path('', index, name="index"),
-	path('sp/', sp_index),
 	path('<int:question_id>/', detail)
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
 from .models import Question
 
 
 def index(request):
-	return HttpResponse("Hello World from a viewe")
-
-def sp_index(request):
-	return HttpResponse("You are browsing polls/sp/")	
+    return HttpResponse("Hello World from a view")
 
 def detail(request, question_id):
-	questions = Question.objects.filter(id=question_id)
-	text = questions[0].question_text
-	return HttpResponse(
-		"You are looking at {}".format(text)
-	)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(
+        request,
+        "polls/detail.html",
+        {"question": question}
+    )
+
