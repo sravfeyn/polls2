e5619f8f5aa2b005d34ed750a5f1efd2af1969a6
Sravan Reddy <sravfeyn@gmail.com>
2018-04-23 17:57:19 +0530

    tabs

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ class Question(models.Model):
     published_date = models.DateTimeField('date published')
 
     def __str__(self):
-		return self.question_text
+        return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -14,4 +14,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-		return self.choice_text
+        return self.choice_text
