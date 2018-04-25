6285e4bb6a99e87962f65c7a0d9006a8af43165b
Sravan Reddy <sravfeyn@gmail.com>
2018-04-25 12:11:38 +0530

    protect all views from public

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
@@ -5,6 +6,7 @@ from django.urls import reverse
 from .models import Question, Choice
 
 
+@login_required
 def index(request):
     return render(
         request,
@@ -13,6 +15,7 @@ def index(request):
     )
 
 
+@login_required
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(
@@ -21,6 +24,8 @@ def detail(request, question_id):
         {"question": question}
     )
 
+
+@login_required
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -40,6 +45,7 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
+@login_required
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
--- polls_site2/urls.py
+++ polls_site2/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	path('polls/', include("polls.urls")),
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='polls/login.html'), name='login'),
+    path('logout/', auth_views.logout, name='logout'),
 ]
