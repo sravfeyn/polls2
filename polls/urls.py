ade65280a9ab06dfa29b246dc5588171e3cd5ead
Sravan Reddy <sravfeyn@gmail.com>
2018-04-25 12:08:57 +0530

    add login and logout views

--- polls/urls.py
+++ polls/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
 	path('', index, name="index"),
 	path('<int:question_id>/', detail, name="detail"),
 	path('<int:question_id>/vote/', vote, name="vote"),
-	path('<int:question_id>/results/', results, name="results")
+	path('<int:question_id>/results/', results, {'template_name': 'polls/login.html'}, name="results")
 ]
